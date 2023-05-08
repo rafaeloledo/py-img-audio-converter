936135391b4b23bb02cc59967bb6877ec92cca7f
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-05-07 21:50:27 -0300

    chore(py): update mp4tom4a.py
    change the print to include file extension

--- mp4tom4a.py
+++ mp4tom4a.py
@@ -5,7 +5,7 @@ if (VideoFileClip is None):
     print("moviepy not found: pip install moviepy")
 
 if len(sys.argv) < 3:
-    print("usage: python mp4tom4a.py input_file output_file")
+    print("usage: python mp4tom4a.py input_file.mp4 output_file.m4a")
     sys.exit(1)
 
 input_file = sys.argv[1]
