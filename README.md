dbf628f3e9cf5480f71d46ef6eaafff00a741115
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-05-07 21:40:22 -0300

    feat(py): first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+### Dependencies
+
+- ``python``
+  - https://www.python.org/
+- ``moviepy``
+  - https://zulko.github.io/moviepy/
+- ``ffmpeg``
+  - https://zulko.github.io/moviepy/
+- ``scoop``
+  - https://scoop.sh/
+
+currently tested on ``windows``
+
+### Usage
+
+```
+python mp4tom4a.py input_file.mp4 output_file.m4a
+```
+
+### Quickstart
+
+On Windows:
+
+```
+pip install moviepy
+```
+```
+scoop install ffmpeg
+```
+
+On Linux:
+
+- Install the deps with your distro's package manager. :)
---
+++ mp4tom4a.py
@@ -0,0 +1,24 @@
+import sys
+from moviepy.editor import VideoFileClip
+
+if (VideoFileClip is None):
+    print("moviepy not found: pip install moviepy")
+
+if len(sys.argv) < 3:
+    print("usage: python mp4tom4a.py input_file output_file")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+clip = VideoFileClip(input_file)
+
+audio = clip.audio
+
+audio_codec = "aac"
+
+audio.write_audiofile(output_file, codec=audio_codec)
+
+clip.close()
+
+print(f"Successfully converted {input_file} to {output_file}.")
